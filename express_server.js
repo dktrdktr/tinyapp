887870d19be44a851cc033df2ac36310a89f9c6d
Donatas Tubutis <dotubutis@gmail.com>
2022-01-25 20:01:46 +0000

    added login form to the header partial

--- express_server.js
+++ express_server.js
@@ -4,6 +4,8 @@ const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
