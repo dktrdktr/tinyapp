251440818ed2b654b635634a79c5b41f5f37bae4
Donatas Tubutis <dotubutis@gmail.com>
2022-01-26 19:05:06 +0000

    added registration template

--- express_server.js
+++ express_server.js
@@ -79,6 +79,13 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("register", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
